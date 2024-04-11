8efa46333ce0ad23d90b291fd276e63aa8f68688
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-12 01:11:44 +0200

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,8 +27,9 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+  def __init__(self, fields: typing.List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """Format the log message"""
