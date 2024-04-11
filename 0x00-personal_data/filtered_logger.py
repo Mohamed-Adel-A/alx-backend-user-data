34f192ff67471139142381938a567c0285b96e79
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-12 01:00:23 +0200

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,8 @@ import re
 import typing
 
 
-def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: typing.List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Replace occurrences of certain field values in a log message.
     """
@@ -15,12 +16,3 @@ def filter_datum(fields: typing.List[str], redaction: str, message: str, separat
         pattern = f'{re.escape(field)}=.+?{re.escape(separator)}'
         message = re.sub(pattern, f'{field}={redaction}{separator}', message)
     return message
-
-
-if __name__ == "__main__":
-    fields = ["password", "date_of_birth"]
-    messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-                "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-    separator = ";"
-    for message in messages:
-        print(filter_datum(fields, 'xxx', message, separator))
