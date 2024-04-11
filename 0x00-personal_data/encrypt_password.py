19929da15559dcb70d5310b9bd898a343019ed05
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-11 15:17:40 +0200

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,26 @@
-#!/usr/bin/python3
+"""
+enccrypt_password
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate a password against its hashed version.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+
+if __name__ == "__main__":
+    password = "MyAmazingPassw0rd"
+    hashed_password = hash_password(password)
+    print(hashed_password)
+    print(is_valid(hashed_password, password))
