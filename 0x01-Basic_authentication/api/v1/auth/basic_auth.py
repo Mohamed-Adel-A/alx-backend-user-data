6d95d496f287d81ab992ee676864d0e471ffe388
MDev <email@example.com>
2024-04-13 01:10:09 +0200

    update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,19 +32,24 @@ class BasicAuth(Auth):
             return None
         return authorization_header.split(" ")[1]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
         """Decode the Base64 authorization header.
         
         Args:
-            base64_authorization_header (str): The Base64 authorization header string.
+            base64_authorization_header (str):
+            The Base64 authorization header string.
         
         Returns:
-            str: The decoded value as UTF8 string, or None if decoding fails.
+            str: The decoded value as UTF8 string,
+            or None if decoding fails.
         """
-        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+        if base64_authorization_header is None or \
+                not isinstance(base64_authorization_header, str):
             return None
+        
         try:
             decoded_bytes = base64.b64decode(base64_authorization_header)
             return decoded_bytes.decode('utf-8')
-        except:
+        except base64.binascii.Error:
             return None
