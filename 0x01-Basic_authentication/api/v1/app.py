890acdc0a65554541304cc45bcf69b074f1b92c8
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-12 10:06:13 +0200

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,27 +1,53 @@
 #!/usr/bin/env python3
+""" Module for app views
 """
-Route module for the API
-"""
-from os import getenv
+from flask import Flask, jsonify, request, abort
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
-
+from models.user import User
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
+@app.before_request
+def before_request():
+    """ Filter each request """
+    from os import getenv
+
+    auth = None
+    if getenv("AUTH_TYPE") == "auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+    elif getenv("AUTH_TYPE") == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+
+    if auth is not None:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+
+
+@app.errorhandler(401)
+def unauthorized(e):
+    """ Error handler for 401 status code """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    """ Error handler for 403 status code """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.route('/api/v1/status', methods=['GET'])
+def status() -> str:
+    """ Returns a JSON """
+    return jsonify({"status": "OK"})
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host="0.0.0.0", port="5000")
