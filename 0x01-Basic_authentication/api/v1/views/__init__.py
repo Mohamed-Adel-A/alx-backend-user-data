a2aa869576351529f093f475ed131f3235004b7c
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-12 10:06:45 +0200

    Update __init__.py

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
+""" Package initializer for API views
 """
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-from api.v1.views.users import *
-
-User.load_from_file()
