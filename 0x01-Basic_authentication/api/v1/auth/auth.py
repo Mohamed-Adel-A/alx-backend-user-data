585755dfdd3de84f5dd39317342a232ca01862fb
MDev <email@example.com>
2024-04-13 01:58:09 +0200

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,19 +17,18 @@ class Auth:
                                or (len(excluded_paths) == 0))):
             return True
 
-        new_excluded = list()
+        if path[-1] != '/':
+            path += '/'
+
         for excluded_path in excluded_paths:
             if excluded_path.endswith('*'):
                 prefix = excluded_path[:-1]
-                new_excluded.append(prefix)
-            else:
-                new_excluded.append(excluded_path)
+                if path.startswith(prefix):
+                    return False
+            elif path == excluded_path:
+                return False
 
-        if path[-1] != '/':
-            path += '/'
-        if path not in new_excluded:
-            return True
-        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
