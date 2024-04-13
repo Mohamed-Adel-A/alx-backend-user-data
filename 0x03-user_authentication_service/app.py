6601941902b57d8ad972dead1f287aa54a5d3aa6
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-04-13 02:42:55 +0200

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Main Flask app
+"""
+from flask import Flask, jsonify, request, abort, make_response
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """
+    Index route
+    """
+   
