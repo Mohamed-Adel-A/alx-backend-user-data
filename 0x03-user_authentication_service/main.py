a475b62daf8b4726024a49191e51aba871dee3fd
MDev <email@example.com>
2024-04-13 22:14:22 +0200

    Task 20. End-to-end integration test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,78 +2,59 @@
 """
 main.py
 """
-from flask import jsonify, request, abort, make_response
-from auth import Auth
-from app import app
+import requests
 
-AUTH = Auth()
+BASE_URL = "http://localhost:5000"
 
 
-@app.route('/register', methods=['POST'])
-def register():
-    """
-    Register route
-    """
-    if not request.json or 'email' not in request.json or 'password' not in request.json:
-        abort(400)
-    try:
-        email = request.json['email']
-        password = request.json['password']
-        user = AUTH.register_user(email, password)
-        return jsonify({'message': f"User {user.email} successfully registered"}), 201
-    except ValueError as e:
-        return jsonify({'error': str(e)}), 400
+def register_user(email: str, password: str) -> None:
+    data = {"email": email, "password": password}
+    response = requests.post(f"{BASE_URL}/users", data=data)
+    assert response.status_code == 200
 
-@app.route('/login', methods=['POST'])
-def login():
-    """
-    Login route
-    """
-    if not request.json or 'email' not in request.json or 'password' not in request.json:
-        abort(400)
-    email = request.json['email']
-    password = request.json['password']
-    if not AUTH.valid_login(email, password):
-        abort(401)
-    session_id = AUTH.create_session(email)
-    if not session_id:
-        abort(500)
-    return jsonify({'session_id': session_id}), 200
 
-@app.route('/logout', methods=['POST'])
-def logout():
-    """
-    Logout route
-    """
-    if not request.json or 'session_id' not in request.json:
-        abort(400)
-    session_id = request.json['session_id']
-    user = AUTH.get_user_from_session_id(session_id)
-    if not user:
-        abort(401)
-    AUTH.destroy_session(user.id)
-    return jsonify({'message': 'Logout successful'}), 200
+def log_in_wrong_password(email: str, password: str) -> None:
+    data = {"email": email, "password": password}
+    response = requests.post(f"{BASE_URL}/sessions", data=data)
+    assert response.status_code == 401
 
-@app.errorhandler(400)
-def bad_request(error):
-    """
-    Error handler for bad requests
-    """
-    return make_response(jsonify({'error': 'Bad request'}), 400)
 
-@app.errorhandler(401)
-def unauthorized(error):
-    """
-    Error handler for unauthorized access
-    """
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+def profile_unlogged() -> None:
+    response = requests.get(f"{BASE_URL}/profile")
+    assert response.status_code == 403
 
-@app.errorhandler(500)
-def internal_server_error(error):
-    """
-    Error handler for internal server errors
-    """
-    return make_response(jsonify({'error': 'Internal server error'}), 500)
+
+def log_in(email: str, password: str) -> str:
+    data = {"email": email, "password": password}
+    response = requests.post(f"{BASE_URL}/sessions", data=data)
+    assert response.status_code == 200
+    return response.cookies.get("session_id")
+
+
+def profile_logged(session_id: str) -> None:
+    cookies = {"session_id": session_id}
+    response = requests.get(f"{BASE_URL}/profile", cookies=cookies)
+    assert response.status_code == 200
+    assert response.json()["email"] == "guillaume@holberton.io"
+
+
+def log_out(session_id: str) -> None:
+    cookies = {"session_id": session_id}
+    response = requests.delete(f"{BASE_URL}/sessions", cookies=cookies)
+    assert response.status_code == 200
+
+
+def reset_password_token(email: str) -> str:
+    data = {"email": email}
+    response = requests.post(f"{BASE_URL}/reset_password", data=data)
+    assert response.status_code == 200
+    return response.json()["reset_token"]
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    data = {"email": email, "reset_token": reset_token, "new_password": new_password}
+    response = requests.put(f"{BASE_URL}/reset_password", data=data)
+    assert response.status_code == 200
 
 
 EMAIL = "guillaume@holberton.io"
@@ -82,7 +63,6 @@ NEW_PASSWD = "t4rt1fl3tt3"
 
 
 if __name__ == "__main__":
-
     register_user(EMAIL, PASSWD)
     log_in_wrong_password(EMAIL, NEW_PASSWD)
     profile_unlogged()
