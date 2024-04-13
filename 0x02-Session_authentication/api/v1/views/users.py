4fbf2181253e117deb5e2c095fff2256e935742f
MDev <email@example.com>
2024-04-13 13:07:18 +0200

    update view user

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -44,6 +44,13 @@ def delete_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
+        else:
+            return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -120,13 +127,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
-
-@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
-def get_current_user():
-    """ Retrieves the authenticated User object
-    """
-    if request.current_user is None:
-        abort(404)
-
-    return jsonify(request.current_user.to_dict()), 200
