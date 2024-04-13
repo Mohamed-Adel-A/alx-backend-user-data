f79a319d303fc68a513b69829d72c34a0bb6ccd5
MDev <email@example.com>
2024-04-13 13:21:04 +0200

    update session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Session authentication module
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session authentication class
+    """
+    pass
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,10 +1,1 @@
 #!/usr/bin/env python3
-""" Session authentication module
-"""
-from api.v1.auth.auth import Auth
-
-
-class SessionAuth(Auth):
-    """ Session authentication class
-    """
-    pass
