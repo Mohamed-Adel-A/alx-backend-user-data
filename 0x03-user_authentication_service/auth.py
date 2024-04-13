cbe049ece1ac593c59d45bf7c73f18a256a86259
MDev <email@example.com>
2024-04-13 22:15:46 +0200

    doc get_reset_password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,8 +94,7 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email):
-        """
-        Generate reset password token useing email
+        """Generate reset password token useing email
         """
         try:
             user = self._db.find_user_by(email=email)
