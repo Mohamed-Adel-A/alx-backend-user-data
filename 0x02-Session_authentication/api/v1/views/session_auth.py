b5e41d15dc2d012e545299e82319e23bb1998277
MDev <email@example.com>
2024-04-13 15:06:04 +0200

    update login

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -11,6 +11,9 @@ from api.v1.views import app_views
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
+    """
+    login route
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
