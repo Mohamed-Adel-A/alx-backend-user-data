606dd2d6bfff14acaaa7386f6b82a51a0c4292c0
MDev <email@example.com>
2024-04-13 19:57:24 +0200

    Task 1. Reading from Redis and recovering original type

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,24 +1,15 @@
-#!/usr/bin/env python3
-"""
-DB module
-"""
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from user import User
-
-Base = declarative_base()
+from user import User, Base
 
 
 class DB:
-    """
-    DB class
+    """DB class
     """
 
     def __init__(self) -> None:
-        """
-        Initialize a new DB instance
+        """Initialize a new DB instance
         """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
@@ -27,39 +18,17 @@ class DB:
 
     @property
     def _session(self):
-        """
-        Memoized session object
+        """Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
-        """
-        Add user to the database
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
-
-    def find_user_by(self, **kwargs):
-        """
-        Find user by attribute
-        """
-        return self._session.query(User).filter_by(**kwargs).first()
-
-    def update_user(self, user_id, **kwargs):
-        """
-        Update user by ID
-        """
-        user = self.find_user_by(id=user_id)
-        if not user:
-            raise ValueError("User not found")
-        for key, value in kwargs.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
-            else:
-                raise ValueError("Invalid attribute")
-        self._session.commit()
